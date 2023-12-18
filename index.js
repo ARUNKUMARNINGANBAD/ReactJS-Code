21faddd6022c649591605c74eee297779f5a0415
ARUNKUMARNINGANBAD <arunkumarninganbad@gmail.com>
2023-12-18 13:19:57 +0530

    good

---
+++ index.js
@@ -0,0 +1,14 @@
+//export default js
+
+// const head =  React.createElement("div",{},
+// [React.createElement("h1",{},"am s1"),React.createElement("h2",{},"am s1")]);
+
+const head =  React.createElement("h1",{},
+"hello Arun !!");
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(head);
+
+
